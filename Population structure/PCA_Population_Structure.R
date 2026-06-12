8f21c66d997d49249fc4b23044ea8e326c5b10e4
raquelthuss <raquelthuss@gmail.com>
2026-06-12 20:37:44 +0200

    Add files via upload

--- Population structure/PCA_Population_Structure.R
+++ Population structure/PCA_Population_Structure.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 library(readODS)
+library(stringr)
 
 rm(list=ls())
 setwd("~/Desktop/1.LAB/TFM/structure/FAST_STRUCTURE_het_pruned_2/")
@@ -16,10 +17,16 @@ setwd("~/Desktop/1.LAB/TFM/structure/FAST_STRUCTURE_het_pruned_2/")
 pca <- read.table("dataset_pca.eigenvec", header = F)
 eigenval <- read.table("dataset_pca.eigenval", header = F)$V1
 
-colnames(pca) <- c("FID", "IID", paste0("PC", 1:(ncol(pca)-2)))
+colnames(pca) <- c("FID", "Taxa", paste0("PC", 1:(ncol(pca)-2)))
 
 # Read coancestry matrix (Q-matrix) for the optimal K value
 qmat <- read.table("Qmatrix_K3.txt", header = T, sep = "\t")
+
+# Read geographic distribution data
+meta_data <- read_ods("meta.ods", sheet = 2)
+meta_data <- meta_data[meta_data$Species == "G. soja", c("Orig_number", "Country")]
+colnames(meta_data) <- c("Taxa", "Country")
+meta_data$Taxa <- as.numeric(meta_data$Taxa)
 
 ###########################################################################
 # 2. Plot
@@ -33,16 +40,29 @@ qmat$Cluster <- apply(qmat[, paste0("Cluster", 1:3)], 1, function(x) {
   paste0("K", which.max(x))
 })
 
+pca <- pca %>% 
+  left_join(meta_data, by = "Taxa")
+
+# Save the first letter of each country for subsequent plotting
+pca$First_letter <- abbreviate(pca$Country, minlength = 2)
+
 # Verify same order of rows (ID)
 all(pca$IID == qmat$Taxa)
 
+# Legend to indicate de country of origin from every sample
+legend_text <- paste(pca$First_letter, pca$Country, sep = " = ", collapse = "\n")
+
 # Plot
 ggplot(pca, aes(x = PC1, y = PC2, color = qmat$Cluster)) + 
-  geom_point(size = 2.5, alpha = 0.8) + 
+  geom_point(size = 2.5, alpha = 0.8) +
+  geom_text_repel(aes(label = First_letter), 
+                  size = 3,
+                  max.overlaps = 25) +
   scale_color_manual(values = c("K1" = "#4e79a7",
                                 "K2" = "#e15759",
                                 "K3" = "#59a14f")) +
+  
   labs(x = paste0("PC1 (", CP_var[1], "%)"),
        y = paste0("PC2 (", CP_var[2], "%)"),
        color = "Cluster") +
-  theme_bw()
+  theme_bw() 
